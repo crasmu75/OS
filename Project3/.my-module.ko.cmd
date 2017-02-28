df18cc0e9cd563a739a35bb3a4848d3ffc742fcb
Camille <crasmu75@bruinmail.slcc.edu>
2017-02-27 17:28:58 -0700

    Project 3

---
+++ Project3/.my-module.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/Documents/OS/Project3/my-module.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-45-generic/scripts/module-common.lds --build-id  -o /home/student/Documents/OS/Project3/my-module.ko /home/student/Documents/OS/Project3/my-module.o /home/student/Documents/OS/Project3/my-module.mod.o
---
+++ Project3/.tmp_versions/my-module.mod
@@ -0,0 +1,2 @@
+/home/student/Documents/OS/Project3/my-module.ko
+/home/student/Documents/OS/Project3/my-module.o
